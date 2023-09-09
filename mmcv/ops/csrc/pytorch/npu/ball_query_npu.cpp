a5d23118faabc12a596d33c96bf5314a0f2cd148
sunyinlei <ginray0215@gmail.com>
2023-09-09 19:01:04 +0800

    [Feature] Add the support of BallQuery op for Ascend device

--- mmcv/ops/csrc/pytorch/npu/ball_query_npu.cpp
+++ mmcv/ops/csrc/pytorch/npu/ball_query_npu.cpp
@@ -8,27 +8,28 @@ void ball_query_forward_npu(int b, int n, int m, float min_radius,
                              const Tensor new_xyz, const Tensor xyz,
                              Tensor idx) {
   int64_t nsample_i64 = nsample;
-  at::Tensor new_xyz_transpose = at::zeros_like(new_xyz);
-  at::Tensor xyz_transpose = at::zeros_like(xyz);
 
   // transpose new_xyz from [B, M, 3] to [M, B, 3]
-  new_xyz_transpose = new_xyz.transpose(0, 1)
+  at::Tensor new_xyz_transpose = new_xyz.transpose(0, 1);
 
   // transpose xyz from [B, N, 3] to [B, 3, N]
-  xyz_transpose = xyz.transpose(1, 2)
+  at::Tensor xyz_transpose = xyz.transpose(1, 2);
+
+  // transpose idx from [B, M, nsample] to [M, B, nsample]
+  at::Tensor idx_transpose = NpuUtils::format_contiguous(idx.transpose(0, 1));
 
   OpCommand cmd;
   cmd.Name("BallQuery")
       .Input(xyz_transpose)
       .Input(new_xyz_transpose)
-      .Output(idx)
+      .Output(idx_transpose)
       .Attr("min_radius", min_radius)
       .Attr("max_radius", max_radius)
       .Attr("sample_num", nsample_i64)
       .Run();
 
-  // transpose idx from [M, B, nsample] to [B, M, nsample]
-  idx = idx.transpose(0,1);
+  idx_transpose = NpuUtils::format_contiguous(idx_transpose.transpose(0, 1));
+  idx.copy_(idx_transpose);
 }
 
 
